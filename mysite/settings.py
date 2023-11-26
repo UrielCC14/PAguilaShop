7aed6529d475f392af5f5b022c11ca48e016690b
UrielCC <calogerou1406@gmail.com>
2023-11-26 14:59:36 -0600

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,3 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WHITENOISE_AUTOREFRESH = True  # o False, prueba ambos
+WHITENOISE_USE_FINDERS = True
