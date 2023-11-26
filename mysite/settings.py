97827e8410d97b24ab4b204ea1af805621e180f1
UrielCC <calogerou1406@gmail.com>
2023-11-26 12:06:02 -0600

    nueva

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,8 +145,8 @@ if not DEBUG:
 LOGIN_URL = 'SignIn/'
 
 import os
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'IMG')
+MEDIA_URL = '/static/IMG/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
